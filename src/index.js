6213e63e8fd9859095810635865e30277df21ba2
Mackenzie Clark <mackenzie_a_clark@homedepot.com>
2018-02-06 15:06:55 -0800

    Fix typo

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ class Game extends React.Component {
     render() {
         const history = this.state.history;
         const current = history[history.length - 1];
-        const winner = calculateWinner(this.state.squares);
+        const winner = calculateWinner(current.squares);
 
         let status;
         if (winner) {
